1b860004899b7c52373e61fb29de99e670e4129b
Owais1456 <159907792+Owais1456@users.noreply.github.com>
2024-09-11 21:22:29 +0500

    reduce
    reducs

---
+++ New folder (15)/src/App.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Card from './Components/Card'
+
+import Login from './Screen/Login'
+import Header from './Components/Header'
+import Son from './Components/Son'
+import Application from './Components/Application'
+import Footer from './Components/Footer'
+import Profile from './Components/Profile'
+import Products from './Components/Products'
+import Des from './Components/Des'
+import Owais from './Components/Owais'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Header/>
+    <Login/>
+    <Card/>
+    <Son/>
+    <Application/>
+    <Footer/>
+    <Profile/>
+    <Products/>
+    <Des/>
+    <Owais/>
+    
+    
+    </div>
+  )
+}
+
+export default App
---
+++ New folder (15)/src/Components/Card.jsx
@@ -0,0 +1,95 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold text-gray-800 bg-yellow-400 text-center ]'>Welcome to our React App</h1>
+    
+    <div className='mt-[30px] w-[880px] text-center grid grid-cols-4 gap-6 mx-auto'>
+      
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img  src="https://img.freepik.com/free-photo/green-park-view_1417-1492.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1725667200&semt=ais_hybrid" alt="" 
+          className='h-60 hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://plus.unsplash.com/premium_photo-1673306778968-5aab577a7365?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8YmFja2dyb3VuZCUyMGltYWdlfGVufDB8fDB8fHww" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://static.vecteezy.com/system/resources/thumbnails/026/365/937/small_2x/beautiful-blurred-green-nature-background-ai-generated-photo.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://i.pinimg.com/originals/58/bd/4f/58bd4fc9ebfccc1f2de419529bbf1a12.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://m.media-amazon.com/images/I/71+17bVYHxL._AC_UF1000,1000_QL80_.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://m.media-amazon.com/images/I/71NQ8HuXTmL._AC_UF1000,1000_QL80_.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://m.media-amazon.com/images/I/71oEZttmFlL._AC_UF1000,1000_QL80_.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+      <div className='shadow-lg'>
+        <div className='overflow-hidden'> 
+          <img src="https://m.media-amazon.com/images/I/71HsnMd3bkL._AC_UF1000,1000_QL80_.jpg" alt="" 
+          className='h-60  hover:scale-125 duration-1000'/>
+
+        </div>
+        <h3>our Buildng</h3> 
+        <p className='text[10px] py-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, velit.</p>
+        
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default Card
